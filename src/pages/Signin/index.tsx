c813d88d90ec06f5527db0d32e6758c9c0f7a3f7
Caio ubuntu 2020 <caiocesarca@gmail.com>
2020-08-01 21:01:17 -0300

    Input

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -6,30 +6,28 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const Signin: React.FC = () => (
-  <>
-    <Container>
-      <Content>
-        <img src={LogoImg} alt="GoBarber" />
-        <form>
-          <h1>Faça seu login</h1>
-          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
-          <Button type="submit"> Entrar </Button>
-          <a href="esqueci">Esqueci minha senha</a>
-        </form>
-        <a href="criarconta">
-          <FiLogIn />
-          Criar conta
-        </a>
-      </Content>
-      <Background />
-    </Container>
-  </>
+  <Container>
+    <Content>
+      <img src={LogoImg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu login</h1>
+        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit"> Entrar </Button>
+        <a href="esqueci">Esqueci minha senha</a>
+      </form>
+      <a href="criarconta">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
 );
 
 export default Signin;
